c2e010b1e0bd4e159458b77ca06222e3d83443c0
Vivek Kumar <vh118854@gmail.com>
2025-09-18 15:13:01 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-4xl font-bold bg-blue-500'>
+      Hi there
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Navbar Page 
+    </div>
+  )
+}
+
+export default Navbar
